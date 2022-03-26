975ceef45d8603f15247a623c4073aa9dff4a7e5
scama01 <64895020+scama01@users.noreply.github.com>
2022-03-26 22:13:54 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vrp from "vite-raw-plugin";
 import pnr from "@rollup/plugin-node-resolve";
-import pb from "@rollup/plugin-babel";
-import pcss from "rollup-plugin-postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +10,14 @@ export default defineConfig({
     vrp({
       fileRegex: /\.md$/,
     }),
-    pnr(),
   ],
   build: {
     rollupOptions: {
       output: {
         minifyInternalExports: true,
       },
+      plugins: [pnr()],
     },
+    chunkSizeWarningLimit: 1600,
   },
 });
